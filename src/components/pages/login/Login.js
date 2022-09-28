6c5dfae2c2b29db438ea714ef736e860221b4060
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-09-28 11:24:51 +0200

    helmet login

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -3,7 +3,7 @@ import { H1, P } from "../../common.styles/DisplayText";
 import Section from "../../common.styles/Section";
 import { FaLock } from 'react-icons/fa';
 import LoginForm from "./LoginForm";
-import useDocumentTitle from "../../../hooks/useDocumentTitle";
+import SEO from "../../SEO";
 
 const Div = styled.div`
   background-color: ${({ theme }) => theme.colors.primaryColor};
@@ -13,25 +13,30 @@ const Div = styled.div`
 `;
 
 const Login = () => {
-  useDocumentTitle('Holidaze | Login');
 
   return (
-    <main>
-      <Section backgroundColorLight>
-        <H1 title="Login" />
-        <P paragraph="Login for admins of Holidaze and hotel owners." />
-      </Section>
-      <Section>
-        <Div>
-          <FaLock color="white" size="3rem" />
-          <P lightColor paragraph="Welcome back" uppercase />
-          <P lightColor paragraph="Please login to your account to lorem ipusm." />
-        </Div>
-      </Section>
-      <Section>
-        <LoginForm />
-      </Section>
-    </main>
+    <>
+      <SEO
+        title="Login"
+        description="Login page for Holidaze administration"
+        keywords="bergen, hotel, login" />
+      <main>
+        <Section backgroundColorLight>
+          <H1 title="Login" />
+          <P paragraph="Login for admins of Holidaze and hotel owners." />
+        </Section>
+        <Section>
+          <Div>
+            <FaLock color="white" size="3rem" />
+            <P lightColor paragraph="Welcome back" uppercase />
+            <P lightColor paragraph="Please login to your account to lorem ipusm." />
+          </Div>
+        </Section>
+        <Section>
+          <LoginForm />
+        </Section>
+      </main>
+    </>
   )
 }
 
